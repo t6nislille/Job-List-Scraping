df99ec9d703a4536eb29e2e7fa0aff1509ef0ea4
Tõnis <tonislille@gmail.com>
2024-01-23 12:09:43 +0200

    Get job description and company name from job listings

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
 from bs4 import BeautifulSoup
 import requests
 
-html_text = requests.get("https://www.cvkeskus.ee/toopakkumised?op=search&search%5Bjob_salary%5D=3&ga_track=results&search%5Bcategories%5D%5B%5D=8&search%5Bkeyword%5D=&badge%5Bkeyword%5D=on&search%5Bexpires_days%5D=&search%5Bjob_lang%5D=&search%5Bsalary%5D=").text
+url = "https://www.cvkeskus.ee/toopakkumised?op=search&search%5Bjob_salary%5D=3&ga_track=results&search%5Bcategories%5D%5B%5D=8&search%5Bkeyword%5D=&badge%5Bkeyword%5D=on&search%5Bexpires_days%5D=&search%5Bjob_lang%5D=&search%5Bsalary%5D="
+html_text = requests.get(url).text
 soup = BeautifulSoup(html_text, 'lxml')
-job = soup.find('article', attrs={'data-component':'jobad'})
+jobs = soup.find_all('article', attrs={'data-component':'jobad'})
+# Find company names and job description
+for job in jobs:
+    company_name = job.find('span', class_= 'job-company mr-5').text.replace(' ', '')
+    #jobadUrl = job.find('a', class_= 'jobad-url').text.replace(' ', '')
+    jobDescription = job.find('h2', class_='xl:text-xl').text.replace(' ', '')
 
-company_name = job.find('span', class_= 'job-company mr-5').text.replace(' ', '')
+    print(f'''
+    Company Name: {company_name}
+    Job description: {jobDescription}
+    ''')
 
-skills = job.find()
 
-print(company_name)
